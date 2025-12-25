2e9caac8f090b8747a5c8964b7dd6c80bbfa75aa
winsznx <timjosh507@gmail.com>
2025-12-25 03:17:45 +0100

    refactor: Introduce TypeScript types for components and update UI styling in WalletConnectButton.

--- web/src/app/components/RootLayout.tsx
+++ web/src/app/components/RootLayout.tsx
@@ -1,17 +1,21 @@
-// @ts-nocheck
-
 "use client";
+import { ReactNode } from "react";
 import { Header } from "@/app/components/Header";
 import { Footer } from "@/app/components/Footer";
 import { Bubble } from "@/app/components/Bubble";
 
-export function RootLayout({ signOut, children }) {
+interface Props {
+  signOut: () => void;
+  children: ReactNode;
+}
+
+export function RootLayout({ signOut, children }: Props) {
   return (
     <div className="relative flex flex-auto overflow-hidden">
       <Bubble position="-top-[560px] -left-[144px]" />
       <Bubble position="-top-[520px] -right-[270px]" />
       <div className="relative flex flex-col w-full">
-        <Header />
+        <Header signOut={signOut} />
 
         <main>
           <div className="mx-auto max-w-6xl p-6 lg:px-8 w-full">
